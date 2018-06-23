08cc13f8430d2bb67702e6de3ecd0a172607b37f
Mihajlo Jovanovic <michael@accusys.co>
2018-06-24 01:50:55 +0200

    min

---
+++ src/AppContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+    render(){
+        return(
+            <div>X</div>
+        )
+    }
+}
+
+export default AppContainer
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = "Te";
+import AppContainer from './AppContainer'
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <AppContainer/>,
   document.getElementById('app')
 );
